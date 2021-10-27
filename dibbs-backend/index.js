17d6a995948c0a5cb77c77764a839ed0b7b4f7ef
Yodamaul <41446808+Yodamaul@users.noreply.github.com>
2021-10-27 15:23:21 +0200

    working firebase

--- dibbs-backend/index.js
+++ dibbs-backend/index.js
@@ -1,14 +1,15 @@
 const Express = require('express');
 const { auth, requiresAuth } = require('express-openid-connect');
-
-var admin = require("firebase-admin");
+var cors = require('cors')
+var admin = require("firebase-admin/app");
+var firebase_auth = require("firebase-admin/auth");
 
 require('dotenv').config();
 
 var serviceAccount = require("./dibbSAK.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.cert(serviceAccount)
 });
 
 
@@ -20,15 +21,19 @@ const config = {
 
 const app = Express();
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+
+app.use(cors())
 
 app.get('/', async (req, res) => {
-    let { token_type, access_token, isExpired, refresh } = req.oidc.accessToken;
-    let user = req.oidc.user;
-    const uid = user.sub.split("|");
 
-    getAuth()
+
+});
+
+
+app.get("/authenticate",
+async (req, res) => {
+  const uid = req.query.uid;
+  firebase_auth.getAuth()
     .createCustomToken(uid)
     .then((customToken) => {
         res.send(customToken);
@@ -37,7 +42,11 @@ app.get('/', async (req, res) => {
         console.log('Error creating custom token:', error);
     });
 
-});
+}
+
+
+);
+
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
--- dibbs-website/src/components/HelloWorld.vue
+++ dibbs-website/src/components/HelloWorld.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-     <div v-if="!$auth.loading">
+    <div v-if="!$auth.loading">
       <!-- show login when not authenticated -->
       <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
       <div v-if="$auth.isAuthenticated">
-
-        
-        NAME: {{ $auth.user.name }} <br>
+        NAME: {{ $auth.user.name }} <br />
         {{ $auth.user.sub.split("|")[2] }}
+        <button @click="fAuth">Auth with Firebase</button>
       </div>
       <!-- show logout when authenticated -->
       <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
@@ -17,23 +16,81 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  methods:{
-    login:function(){
-       this.$auth.loginWithRedirect();
-    },
-       logout:function(){
-       this.$auth.logout({
-        returnTo: window.location.origin
-      });
-    }
+const axios = require("axios");
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  }
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBw8BHbNhODW_2PaQInj2P7gq3SFnYarE8",
+  authDomain: "dibs-bb.firebaseapp.com",
+  projectId: "dibs-bb",
+  storageBucket: "dibs-bb.appspot.com",
+  messagingSenderId: "224485029847",
+  appId: "1:224485029847:web:ee43b1fc78fa4b0a4afd92",
+  measurementId: "G-DN7NSXB7K9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  created: function () {
+    var user = auth.currentUser;
+
+  },
+  methods: {
+    login: function () {
+      this.$auth.loginWithRedirect();
+    },
+
+    fAuth: function () {
+      console.log(this.$auth.user.sub.split("|")[2]);
+      axios
+        .get("http://localhost:3000/authenticate", {
+          params: { uid: 495586848031244299 },
+        })
+        .then(function (response) {
+          try {
+            let token = response.data;
+            firebase
+              .auth()
+              .signInWithCustomToken(token)
+              .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                console.log(user.uid);
+              })
+              .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ...
+              });
+          } catch (e) {
+            window.location.replace("/");
+            return;
+          }
+        });
+    },
+
+    logout: function () {
+      this.$auth.logout({
+        returnTo: "/",
+      });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
